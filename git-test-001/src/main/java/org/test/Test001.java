3b07664acd14c5248ed260a3bdab22b1338e6831
root <root@163.com>
2017-07-24 22:14:35 +0800

    eeee
    eeee

--- git-test-001/src/main/java/org/test/Test001.java
+++ git-test-001/src/main/java/org/test/Test001.java
@@ -4,7 +4,7 @@ public class Test001 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello git");
+		System.out.println("hello gitkkk");
 	}
 
 }
